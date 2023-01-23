f242334710b3f8fd15b98f01a10942da1416cb28
HyperWin <92534013+NoSystemFound@users.noreply.github.com>
2023-01-23 02:03:57 -0800

    Add files via upload

---
+++ URLFileDownloader/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace URLFileDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(5, 5);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PromptText = "URL";
+            this.metroTextBox1.Size = new System.Drawing.Size(359, 19);
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Location = new System.Drawing.Point(5, 30);
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PromptText = "Save file";
+            this.metroTextBox2.Size = new System.Drawing.Size(333, 19);
+            this.metroTextBox2.TabIndex = 1;
+            this.metroTextBox2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(344, 30);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(21, 19);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "...";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(5, 56);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(359, 32);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "Start download";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Visible = false;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(5, 55);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(359, 32);
+            this.metroProgressBar1.TabIndex = 5;
+            this.metroProgressBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroProgressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(371, 94);
+            this.Controls.Add(this.metroProgressBar1);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+    }
+}
+
---
+++ URLFileDownloader/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace URLFileDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string fileSavePath;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroButton3.Enabled = true;
+            metroButton3.Visible = true;
+            metroProgressBar1.Enabled = false;
+            metroProgressBar1.Visible = false;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileSavePath = saveFileDialog1.FileName;
+                metroTextBox2.Text = fileSavePath;
+            }
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            metroButton3.Enabled = false;
+            metroButton3.Visible = false;
+            metroProgressBar1.Enabled = true;
+            metroProgressBar1.Visible = true;
+            WebClient webcl = new WebClient();
+            string sourceFile = metroTextBox1.Text;
+            string destFile = metroTextBox2.Text;
+            webcl.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompleted);
+            webcl.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webcl.DownloadFileAsync(new Uri(sourceFile), destFile);
+        }
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            metroProgressBar1.Value = e.ProgressPercentage;
+        }
+        private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Manager", "File download completed", MessageBoxButtons.OK);
+        }
+    }
+}
